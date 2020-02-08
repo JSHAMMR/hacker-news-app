0e07e52a1f806ccdab00302152599340d8e067bc
Jamil Shamar <jameelshammr@gmail.com>
2020-02-08 22:00:11 +0800

    integrate StoriesListViewController-StoriesPresenter&StoriesListProvider

---
+++ HackerNewsApp/Listing/View/StoriesListViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  HackerNewsApp
+//
+//  Created by Gamil Ali Qaid Shamar on 08/02/2020.
+//  Copyright © 2020 Jamil. All rights reserved.
+//
+
+import UIKit
+
+class StoriesListViewController: UIViewController {
+    @IBOutlet weak var storiesTableView: UITableView!
+    @IBOutlet var storiesListProvider: StoriesListProvider!
+    var storiesPresenter:StoriesPresenter!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.storiesPresenter = self.storiesListPresenter()
+        self.storiesPresenter?.fetchStoriesItems()
+        self.storiesPresenter?.delegate = self
+        
+        self.storiesListProvider.storiesPresenter = self.storiesPresenter
+        self.storiesTableView.delegate = self.storiesListProvider
+        self.storiesTableView.dataSource = self.storiesListProvider
+
+        // Do any additional setup after loading the view.
+    }
+
+    func storiesListPresenter() -> StoriesPresenter {
+           let networkLayer = Network()
+           let storiesViewModel = StoriesViewModel(networkLayer: networkLayer)
+           let storiesPresenter = StoriesPresenter(storiesViewModel: storiesViewModel)
+           return storiesPresenter
+    }
+}
+
+extension StoriesListViewController:StoriesPresenterDelegate {
+    func didFetchStoriesItems(success: Bool) {
+        DispatchQueue.main.async {
+            self.storiesTableView.reloadData()
+        }
+    }
+    
+}
