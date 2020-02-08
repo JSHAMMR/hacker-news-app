3b7fb728314def883f3cba29adf47e92ccc86ec4
Jamil Shamar <jameelshammr@gmail.com>
2020-02-08 11:06:30 +0800

    add public urls values to Constant file

---
+++ HackerNewsApp/Utils/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  HackerNewsApp
+//
+//  Created by Gamil Ali Qaid Shamar on 08/02/2020.
+//  Copyright © 2020 Jamil. All rights reserved.
+//
+public let hackerNewsAPIBaseUrL = "https://hacker-news.firebaseio.com/v0/"
+public let topStoriesAction = "topstories.json?print=pretty"
+
