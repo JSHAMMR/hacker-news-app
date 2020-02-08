44a9c2a4bdb698a0ce290366f4aae80af35a1dc2
Jamil Shamar <jameelshammr@gmail.com>
2020-02-08 11:00:20 +0800

    create Story Model

---
+++ HackerNewsApp/Listing/Models/Story.swift
@@ -0,0 +1,86 @@
+//
+//  Story.swift
+//  HackerNewsApp
+//
+//  Created by Gamil Ali Qaid Shamar on 08/02/2020.
+//  Copyright © 2020 Jamil. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Story
+class Story: Codable {
+    var by: String?
+    var descendants, id: Int?
+    var kids: [Int]?
+    var score: Int?
+    var text: String?
+    var time: Int?
+    var title, type: String?
+
+    init(by: String?, descendants: Int?, id: Int?, kids: [Int]?, score: Int?, text: String?, time: Int?, title: String?, type: String?) {
+        self.by = by
+        self.descendants = descendants
+        self.id = id
+        self.kids = kids
+        self.score = score
+        self.text = text
+        self.time = time
+        self.title = title
+        self.type = type
+    }
+}
+
+// MARK: Story convenience initializers and mutators
+
+extension Story {
+    convenience init(data: Data) throws {
+        let me = try newJSONDecoder().decode(Story.self, from: data)
+        self.init(by: me.by, descendants: me.descendants, id: me.id, kids: me.kids, score: me.score, text: me.text, time: me.time, title: me.title, type: me.type)
+    }
+
+    convenience init(_ json: String, using encoding: String.Encoding = .utf8) throws {
+        guard let data = json.data(using: encoding) else {
+            throw NSError(domain: "JSONDecoding", code: 0, userInfo: nil)
+        }
+        try self.init(data: data)
+    }
+
+    convenience init(fromURL url: URL) throws {
+        try self.init(data: try Data(contentsOf: url))
+    }
+
+    func with(
+        by: String?? = nil,
+        descendants: Int?? = nil,
+        id: Int?? = nil,
+        kids: [Int]?? = nil,
+        score: Int?? = nil,
+        text: String?? = nil,
+        time: Int?? = nil,
+        title: String?? = nil,
+        type: String?? = nil
+    ) -> Story {
+        return Story(
+            by: by ?? self.by,
+            descendants: descendants ?? self.descendants,
+            id: id ?? self.id,
+            kids: kids ?? self.kids,
+            score: score ?? self.score,
+            text: text ?? self.text,
+            time: time ?? self.time,
+            title: title ?? self.title,
+            type: type ?? self.type
+        )
+    }
+
+    func jsonData() throws -> Data {
+        return try newJSONEncoder().encode(self)
+    }
+
+    func jsonString(encoding: String.Encoding = .utf8) throws -> String? {
+        return String(data: try self.jsonData(), encoding: encoding)
+    }
+}
+
+
