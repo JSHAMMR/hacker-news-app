f8c7dbf6ee0a2dc2a9f7672d43825b64fe59708b
Jamil Shamar <jameelshammr@gmail.com>
2020-02-08 21:56:32 +0800

    add Story-url

--- HackerNewsApp/Listing/Models/Story.swift
+++ HackerNewsApp/Listing/Models/Story.swift
@@ -13,21 +13,20 @@ public class Story: Codable {
     var by: String?
     var descendants, id: Int?
     var kids: [Int]?
-    var score: Int?
-    var text: String?
-    var time: Int?
+    var score, time: Int?
     var title, type: String?
+    var url: String?
 
-    init(by: String?, descendants: Int?, id: Int?, kids: [Int]?, score: Int?, text: String?, time: Int?, title: String?, type: String?) {
+    init(by: String?, descendants: Int?, id: Int?, kids: [Int]?, score: Int?, time: Int?, title: String?, type: String?, url: String?) {
         self.by = by
         self.descendants = descendants
         self.id = id
         self.kids = kids
         self.score = score
-        self.text = text
         self.time = time
         self.title = title
         self.type = type
+        self.url = url
     }
 }
 
@@ -36,7 +35,7 @@ public class Story: Codable {
 extension Story {
     convenience init(data: Data) throws {
         let me = try newJSONDecoder().decode(Story.self, from: data)
-        self.init(by: me.by, descendants: me.descendants, id: me.id, kids: me.kids, score: me.score, text: me.text, time: me.time, title: me.title, type: me.type)
+        self.init(by: me.by, descendants: me.descendants, id: me.id, kids: me.kids, score: me.score, time: me.time, title: me.title, type: me.type, url: me.url)
     }
 
     convenience init(_ json: String, using encoding: String.Encoding = .utf8) throws {
@@ -56,10 +55,10 @@ extension Story {
         id: Int?? = nil,
         kids: [Int]?? = nil,
         score: Int?? = nil,
-        text: String?? = nil,
         time: Int?? = nil,
         title: String?? = nil,
-        type: String?? = nil
+        type: String?? = nil,
+        url: String?? = nil
     ) -> Story {
         return Story(
             by: by ?? self.by,
@@ -67,10 +66,10 @@ extension Story {
             id: id ?? self.id,
             kids: kids ?? self.kids,
             score: score ?? self.score,
-            text: text ?? self.text,
             time: time ?? self.time,
             title: title ?? self.title,
-            type: type ?? self.type
+            type: type ?? self.type,
+            url: url ?? self.url
         )
     }
 
