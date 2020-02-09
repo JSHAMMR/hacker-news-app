b3e57f154d9e57dea92250e96db8c32a65a23115
Jamil Shamar <jameelshammr@gmail.com>
2020-02-09 11:41:48 +0800

    create CommentNetworkTest

---
+++ HackerNewsAppTests/Comment Test/CommentNetworkTest.swift
@@ -0,0 +1,38 @@
+//
+//  CommentNetworkTest.swift
+//  HackerNewsAppTests
+//
+//  Created by Gamil Ali Qaid Shamar on 09/02/2020.
+//  Copyright © 2020 Jamil. All rights reserved.
+//
+
+import XCTest
+@testable import HackerNewsApp
+
+class CommentNetworkTest: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testNetworkTopStoriesItems() {
+        let network = Network()
+        let expectation = XCTestExpectation(description: "Download Up to 25 top and new stories")
+
+        network.getComments(commentIds:[121016,121109,121168]) { (comments) in
+            XCTAssertNotNil(comments,"Failed to download comments ")
+            XCTAssertFalse(comments?.count == 0,"comments count is zero")
+            XCTAssertTrue(comments?.count == 3,"Failed to load all comments")
+
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: 10.0)
+
+    }
+
+}
