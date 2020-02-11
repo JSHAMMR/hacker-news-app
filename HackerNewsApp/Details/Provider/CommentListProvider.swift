8aef030dd2539cc6e434bed8b863f1345cb9b775
Jamil Shamar <jameelshammr@gmail.com>
2020-02-12 01:09:36 +0800

    refactor CommentListProvider

--- HackerNewsApp/Details/Provider/CommentListProvider.swift
+++ HackerNewsApp/Details/Provider/CommentListProvider.swift
@@ -14,26 +14,75 @@ class CommentListProvider: NSObject, UITableViewDelegate, UITableViewDataSource 
     var commentListProviderProtocol: CommentListProviderProtocol?
     var commentPresenter:CommentPresenter!
 
-    
+    var comments = [Comment]()
+    let network = Network()
+
+    let colors : [UIColor] = [UIColor(named: "level1")!,UIColor(named: "level2")!,UIColor(named: "level3")!]
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return commentPresenter.getCommentCount()
+        return comments.count
 
     }
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CommentTableViewCell", for: indexPath) as! CommentTableViewCell
-        let comment: Comment = commentPresenter.getComment(index: indexPath.row)
 
-        cell.comment = comment
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let comment: Comment = self.comments[indexPath.row]
+
+    {
+             let cell = tableView.dequeueReusableCell(withIdentifier: "CommentTableViewCell", for: indexPath) as! CommentTableViewCell
+            cell.comment = comment
+        cell.backgroundColor = self.colors[self.checklevel(level: 0, comment: comment)]
         
-        return UITableViewCell()
+        print("level : \(self.checklevel(level: 0, comment: comment) * 20)")
+
+
+        return cell
     }
+    
+    func shiftAppend(index:Int, comments:[Comment]) {
+        var currentIndex = index
+        let shift = self.comments.count - 1
+        let shiftedList = self.comments[index...shift]
+        comments.forEach({ (aComment) in
+            currentIndex = currentIndex + 1
+            self.comments.insert(aComment, at: currentIndex)
+        })
+     
+        print(shiftedList)
+    }
+    func checklevel(level:Int, comment:Comment) -> Int {
+        
+        let levelComment = self.comments.filter{comment.parent == $0.id}
+        if levelComment.count == 0 {
+            return level
+
+        }
+        
+        return checklevel(level: level + 1, comment: levelComment[0])
+    }
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-           let comment: Comment = commentPresenter.getComment(index: indexPath.row)
-           commentListProviderProtocol?.didSelectCell(comment: comment)
+           let comment: Comment = self.comments[indexPath.row]
+        commentListProviderProtocol?.didSelectCell(comment: comment)
+
+        print(self.checklevel(level: 0, comment: comment))
+        
+        if comment.kids != nil {
+            if comment.kids!.count > 0 {
+
+            let sortId = self.comments.filter { $0.id ==  comment.kids![0]}
+
+                if sortId.count == 0 {
+                    network.getComments(commentIds: comment.kids!) { (comments) in
+
+                self.shiftAppend(index: indexPath.row, comments: comments!)
+                tableView.reloadData()
+            }
+                }
+                
+            }
+        }
        }
 
 }
